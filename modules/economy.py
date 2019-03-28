e558012a3550c910988f335795a3fb7fca099905
이이레 <krbingpop@gmail.com>
2019-03-27 22:04:36 -0400

    added slots! spin to win

--- modules/economy.py
+++ modules/economy.py
@@ -23,48 +23,6 @@ class Slots(Enum):
 class Payout:
   def __init__(self, bot):
     self.bot = bot
-    self.payout = {
-      (Slots.gun, Slots.gun, Slots.gun): {
-        "payout": lambda x: x * 200,
-        "output": "You found Ranger Rod! Your bet was multiplied by 200."
-      },
-      (Slots.orange, Slots.orange, Slots.orange): {
-        "payout": lambda x: x * 100,
-        "output": "Three oranges! Your bet was multiplied by 100."
-      },
-      (Slots.grape, Slots.grape, Slots.grape): {
-        "payout": lambda x: x * 60,
-        "output": "Three grapes! Your bet was multiplied by 60."
-      },
-      (Slots.lemon, Slots.lemon, Slots.lemon): {
-        "payout": lambda x: x * 40,
-        "output": "Three lemons! Your bet was multiplied by 40."
-      },
-      (Slots.seven, Slots.seven, Slots.seven): {
-        "payout": lambda x: x * 30,
-        "output": "Three moneybags! Your bet was multiplied by 30."
-      },
-      (Slots.money, Slots.money, Slots.money): {
-        "payout": lambda x: x * 20,
-        "output": "Three dollar bills! Your bet was multiplied by 10."
-      },
-      (Slots.bell, Slots.bell, Slots.bell): {
-        "payout": lambda x: x * 10,
-        "output": "Three bells! Your bet was mutliplied by 10."
-      },
-      (Slots.cherry, Slots.cherry, Slots.cherry): {
-        "payout": lambda x: x * 10,
-        "output": "Three cherries! Your bet was multiplied by 10."
-      },
-      (Slots.cherry, Slots.cherry): {
-        "payout": lambda x: x * 5,
-        "output": "Two cherries! Your bet was multiplied by 5."
-      },
-      (Slots.cherry): {
-        "payout": lambda x: x * 2,
-        "output": "One cherry! Your bet was multiplied by 2."
-      },
-    }
     self.payout_message = discord.Embed(title = "**Slot Payouts**")
     self.payout_message.add_field(name = "{gun.value} {gun.value} {gun.value}".format(**Slots.__dict__), value = "Bet \u00d7 200")
     self.payout_message.add_field(name = "{orange.value} {orange.value} {orange.value}".format(**Slots.__dict__), value = "Bet \u00d7 100", inline = True)
@@ -75,9 +33,6 @@ class Payout:
     self.payout_message.add_field(name = "{cherry.value} {cherry.value} {cherry.value}".format(**Slots.__dict__), value = "Bet \u00d7 10")
     self.payout_message.add_field(name = "{cherry.value} {cherry.value}".format(**Slots.__dict__), value = "Bet \u00d7 5", inline = True)
     self.payout_message.add_field(name = "{cherry.value}".format(**Slots.__dict__), value = "Bet \u00d7 2")
-
-  def get_payout_dict(self):
-    return self.payout
 
   def get_payouts(self):
     return self.payout_message
@@ -93,36 +48,95 @@ class Economy:
     self.payday_amount = 500
     with open("modules/_data/users.json") as json_data:
       self.users = json.load(json_data)
+    self.payout = {
+      "gun": {
+        "payout": 200,
+        "output": "You found Ranger Rod! Your bet was multiplied by 200."
+      },
+      "orange": {
+        "payout": 100,
+        "output": "Three oranges! Your bet was multiplied by 100."
+      },
+      "grape": {
+        "payout": 60,
+        "output": "Three grapes! Your bet was multiplied by 60."
+      },
+      "lemon": {
+        "payout": 40,
+        "output": "Three lemons! Your bet was multiplied by 40."
+      },
+      "seven": {
+        "payout": 30,
+        "output": "Three moneybags! Your bet was multiplied by 30."
+      },
+      "money": {
+        "payout": 20,
+        "output": "Three dollar bills! Your bet was multiplied by 10."
+      },
+      "bell": {
+        "payout": 10,
+        "output": "Three bells! Your bet was mutliplied by 10."
+      },
+      "3cherry": {
+        "payout": 10,
+        "output": "Three cherries! Your bet was multiplied by 10."
+      },
+      "2cherry": {
+        "payout": 5,
+        "output": "Two cherries! Your bet was multiplied by 5."
+      },
+      "1cherry": {
+        "payout": 2,
+        "output": "One cherry! Your bet was multiplied by 2."
+      },
+      "none": {
+        "payout": 0,
+        "output": "No matches! You lost your bet."
+      }
+    }
+
 
   # updates the .json with new values and creates a back-up
   def update(self, user):
     with open("modules/_data/users.json", "w") as json_out:
       json.dump(user, json_out, indent = 2)
 
+
+  # get a dict object of all possible payouts for slots
+  def get_payout_dict(self):  
+    return self.payout
+
+
   # returns the name of the currency
   def get_currency_name(self):
     return self.name
+
 
   # returns the payday value
   def get_payday(self):
     return self.payday_amount
 
+
   # checks if the user is registered within the system
   def is_registered(self, user):
     return user.id in self.users
 
+
   # returns the balance of a specified user
   def get_balance(self, user):
     return self.users[user.id]["balance"]
+
 
   # adds (or subtracts) credits to/from a user
   def add_balance(self, user, credits):
     self.users[user.id]["balance"] = self.get_balance(user) + credits
     self.update(self.users)
 
+
   # checks if the user can pay for a certain action
   def can_spend(self, user, cost):
-    return self.get_balance(user) > cost
+    return self.get_balance(user) >= cost
+
 
   # registers an economy account with the bot
   @commands.command(pass_context = True, description = "Registers an account with the bot")
@@ -136,46 +150,64 @@ class Economy:
       embed = discord.Embed(title = "", description = "You're now registered on my database, {}!".format(ctx.message.author.mention))
     await self.bot.say(embed = embed)
 
+
+  # allows users to give fractional strings as arguments and interprets them into integer values
+  def __interpret_frac(self, ctx, bid):
+    if bid == None:
+      return
+    if bid.isdigit():
+      return int(bid)
+    if bid == "all":
+      return self.get_balance(ctx.message.author)
+    if bid == "half":
+      return self.get_balance(ctx.message.author)//2
+    if bid == "fourth":
+      return self.get_balance(ctx.message.author)//4
+    if bid == "tenth":
+      return self.get_balance(ctx.message.author)//10
+    return
+
+
   # pays out x credits to a user
   @commands.command(pass_context = True, description = "Gives some currency every so often")
   @commands.cooldown(1, 1800, commands.BucketType.user)
   async def payday(self, ctx):
-    if is_registered(ctx.message.author):
+    if self.is_registered(ctx.message.author):
       self.add_balance(ctx.message.author, self.get_payday())
       embed = discord.Embed(title = "", description = "Here's {} {}, {}.".format(self.get_payday(), self.get_currency_name(), ctx.message.author.mention), color = ctx.message.author.color)
     else:
       embed = discord.Embed(title = "", description = "It looks like you aren't registered in the system, {}. Try `!register`".format(ctx.message.author.mention))
     await self.bot.say(embed = embed)
 
-    # cashes in a coupon
-    @commands.command(pass_context = True, description = "Cashes in a coupon")
-    async def coupon(self, ctx, *, coupon_code: str = None):
 
-      # checks for null input
-      if coupon_code == None:
-        embed = discord.Embed(title = "", description = "You can cash in a coupon by using `!coupon <coupon code>`")
+  # cashes in a coupon
+  @commands.command(pass_context = True, description = "Cashes in a coupon")
+  async def coupon(self, ctx, *, coupon_code: str = None):
+
+    # checks for null input
+    if coupon_code == None:
+      embed = discord.Embed(title = "", description = "You can cash in a coupon by using `!coupon <coupon code>`")
+    else:
+
+      # opening the list of coupons
+      with open("modules/_data/coupons.json") as json_coupons:
+        coupons = json.load(json_coupons)
+
+      if coupon_code not in coupons:
+        embed = discord.Embed(title = "", description = "It seems that coupon code is invalid, {}. Double check case sensitivity and make sure this coupon has not been claimed already.".format(ctx.message.author.mention))
       else:
-
-        # opening the list of coupons
-        with open("modules/_data/coupons.json") as json_coupons:
-          coupons = json.load(json_coupons)
-
-        if coupon_code not in coupons:
-          embed = discord.Embed(title = "", description = "It seems that coupon code is invalid, {}. Double check case sensitivity and make sure this coupon has not been claimed already.".format(ctx.message.author.mention))
-        else:
-          value = coupons.pop(coupon_code)
-          self.add_balance(ctx.message.author, value)
-          with open("modules/_data/coupons.json", "w") as json_out_coupon:
-            json.dump(coupons, json_out_coupon, indent = 2)
-          embed = discord.Embed(title = "", description = "Successfully cashed in coupon for {} {}, {}".format(value, self.get_currency_name(), ctx.message.author.mention), color = ctx.message.author.color)
-      await self.bot.say(embed = embed)
+        value = coupons.pop(coupon_code)
+        self.add_balance(ctx.message.author, value)
+        with open("modules/_data/coupons.json", "w") as json_out_coupon:
+          json.dump(coupons, json_out_coupon, indent = 2)
+        embed = discord.Embed(title = "", description = "Successfully cashed in coupon for {} {}, {}".format(value, self.get_currency_name(), ctx.message.author.mention), color = ctx.message.author.color)
+    await self.bot.say(embed = embed)
 
 
   # generates a coupon that can be cashed in for currency
   @perms.is_owner()
   @commands.command(pass_context = True, description = "Generates a coupon")
   async def make_coupon(self, ctx, credits: int = 0):
-
     # checks for null input
     if credits <= 0:
       print("Invalid number of credits to be dispensed.")
@@ -205,7 +237,6 @@ class Economy:
   # transfers credits to another user
   @commands.command(pass_context = True, description = "Transfers currency to another user")
   async def transfer(self, ctx, recipient: discord.Member = None, credits: int = 0):
-    
     # checks if the user is trying to send currency to themselves
     if ctx.message.author == recipient:
       embed = discord.Embed(title = "", description = "You can't send {} to yourself, {}!".format(self.get_currency_name(), ctx.message.author.mention)) 
@@ -238,29 +269,64 @@ class Economy:
 
 
   # plays slots
-  @commands.command(pass_context = True, description = "Play some slots!")
-  async def slots(self, ctx, bid: int = None):
+  @commands.command(pass_context = True, aliases = ["slot"], description = "Play some slots!")
+  async def slots(self, ctx, bid: str = None):
     if not self.is_registered(ctx.message.author):
       embed = discord.Embed(title = "", description = "It looks like you aren't registered in the system, {}. Try `!register`".format(ctx.message.author.mention))
-    elif bid is None:
+      await self.bot.say(embed = embed)
+      return
+    bid = self.__interpret_frac(ctx, bid)
+    if bid is None:
       embed = discord.Embed(title = "", description = "You can play slots by with `!slots <bid greater than 0>`.")
     elif bid <= 0:
       embed = discord.Embed(title = "", description = "You need to wager more than 0 {}, {}.".format(self.get_currency_name(), ctx.message.author.mention))
     elif not self.can_spend(ctx.message.author, bid):
       embed = discord.Embed(title = "", description = "You don't have enough {} for that, {}!".format(self.get_currency_name(), ctx.message.author.mention))
     else:
-      reels = []
+      # generating reels
+      reel = []
+      reel_list = list(Slots)
       for row in range(3):
-        column = []
-        for col in range(3):
-          column.append(random.choice(list(Slots)))
-        reels.append(column)
+        # to make the slots realistic, each column contains 3 contiguous icons, rather than 3 completely randomly popped ones
+        rand_choice = random.randint(0,len(list(Slots))-1)
+        reel_row = []
+        reel_row.append(reel_list[(rand_choice-1)%len(reel_list)].value)
+        reel_row.append(reel_list[rand_choice].value)
+        reel_row.append(reel_list[(rand_choice+1)%len(reel_list)].value)
+        reel_row.append(reel_list[rand_choice].name)
+        reel.append(reel_row)
 
+      # starting to create the embed message
+      embed = discord.Embed(title = "", description = ctx.message.author.mention, color = ctx.message.author.color)
+      embed.add_field(name = "\u3164", value = "{} {} {}\n{} {} {} ⬅\n{} {} {}".format(reel[0][0], reel[1][0], reel[2][0], reel[0][1], reel[1][1], reel[2][1], reel[0][2], reel[1][2], reel[2][2]))
+
+      # grabbing initial information for the final embed message
+      initial_balance = self.get_balance(ctx.message.author)
+
+      # checking for a payout
+      reel_key = [reel[0][3], reel[1][3], reel[2][3]]
+      if reel_key[0] == reel_key[1] == reel_key[2]:
+        payout = self.__payouts(reel_key[0])
+      elif (reel_key[0] == reel_key[1] and reel_key[0] == "cherry") or (reel_key[1] == reel_key[2] and reel_key[1] == "cherry"):
+        payout = self.__payouts("2cherry")
+      elif reel_key[0] == "cherry" or reel_key[1] == "cherry" or reel_key[2] == "cherry":
+        payout = self.__payouts("1cherry")
+      else:
+        payout = self.__payouts("none")
+
+      # paying out
+      final_payout = bid*payout["payout"]
+      self.add_balance(ctx.message.author, -bid)
+      self.add_balance(ctx.message.author, final_payout)
       
-      # embed = discord.Embed(title = "{} {} {}\n{} {} {}\n{} {} {}".format(reels[0][0],reels[0][1],reels[0][2],reels[1][0],reels[1][1],reels[1][2],reels[2][0],reels[2][1],reels[2][2]))
-
-
+      # adding payout to embed
+      embed.add_field(name = "Payout:", value = "{}\n{} ⮕ {} {}".format(payout["output"], initial_balance, self.get_balance(ctx.message.author), self.get_currency_name()), inline = False)
+      embed.set_footer(text = "Your bet of {} {} became {}.".format(bid, self.get_currency_name(), final_payout))
     await self.bot.say(embed = embed)
+
+  def __payouts(self, key):
+    return self.payout[key]
+
 
   # error handler that returns an embed message with the remaining time left on a particular command
   @slots.error
