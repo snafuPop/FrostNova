d1ee54ccba280cb812a240dae9536fd9d85a212d
snafuPop <krbingpop@gmail.com>
2019-05-03 07:24:04 +0000

    completely revamped the dfopedia command

--- modules/wiki.py
+++ modules/wiki.py
@@ -5,15 +5,115 @@ from builtins import bot
 from bs4 import BeautifulSoup
 from flask import Flask
 from titlecase import titlecase
-from random import choice
+from enum import Enum
 import requests
+import aiohttp
 
-# Parses wikipedia pages and pulls some information utilizing BeautifulSoup 4
+class Value(Enum):
+  name = 1
 
 class Wiki(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
+    self.replace = {
+      "weapon type": "Type",
+      "armor weight class": "Weight"
+    }
 
+  @commands.command(aliases = ["dfo", "ndfo"], description = "Pulls a page from the DFO World Wiki")
+  async def dfopedia(self, ctx, *, search_terms: str = None):
+    if search_terms is None:
+      await ctx.send(embed = discord.Embed(title = "", description = "Try searching a page with `!dfopedia <search_term>`, {}.".format(ctx.author.mention)))
+      return
+    search_terms = titlecase(search_terms)
+    term = search_terms.replace(" ", "%20")
+    url = "http://wiki.dfo.world/api.php?action=parse&prop=wikitext&page={}&format=json".format(term.replace(" ","%20"))
+
+    try:
+      async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+          parse = await response.json()
+    except Exception as error:
+      await ctx.send(embed = discord.Embed(title = "", description = "Request failed: {}".format(error)))
+      return
+
+    item = parse["parse"]
+    url = "http://wiki.dfo.world/view/{}".format(item["title"].replace(" ", "_"))
+    text = self.turn_into_dict(item) # split the page's contents into a dict to make it easily navigable
+
+    # getting basic item information
+    embed = discord.Embed(
+      title = titlecase("**Level {} {} {}**".format(text["level"], text["rarity"], text["slot"])),
+      description =
+        self.exists_to_str(text, "weapon type") +
+        self.exists_to_str(text, "armor weight class") +
+        self.exists_to_str(text, "class") +
+        self.exists_to_str(text, "binding") +
+        self.exists_to_str(text, "set"),
+      color = ctx.author.color)
+
+    # getting item's name
+    embed.set_author(
+      name = "{}".format(item["title"]),
+      url = url,
+      icon_url = "{}".format(self.fetchThumbnail(url)))
+
+    info = ""
+
+    # getting weapon information
+    for param in ["weapon physical attack", "weapon magical attack", "independent attack"]:
+      info += self.exists_to_str(text, param)
+    if info:
+      embed.add_field(name = "**Attack:**", value = info)
+      info = ""
+
+    # getting armor information
+    for param in ["equipment physical defense", "equipment magical defense"]:
+      info += self.exists_to_str(text, param)
+    if info:
+      embed.add_field(name = "**Defense:**", value = info, inline = False)
+      info = ""
+
+    # getting stat boost information
+    for param in ["strength", "intelligence", "vitality", "spirit"]:
+      info += self.exists_to_str(text, param, plus = True)
+    for param in ["physical critical chance", "magical critical chance", "attack speed", "movement speed", "casting speed", "hit rate"]:
+      info += self.exists_to_str(text, param, plus = True, percent = True)
+    if info:
+      embed.add_field(name = "**Stat Bonuses:**", value = info, inline = False)
+      info = ""
+
+    # getting elemental boost information
+    info += self.exists_to_str(text, "inflict element")
+    for param in ["all elemental damage", "fire damage", "water damage", "light damage", "shadow damage"]:
+      info += self.exists_to_str(text, param)
+    if info:
+      embed.add_field(name = "**Elemental Stats:**", value = info, inline = False)
+      info = ""
+
+    # getting skill boost information
+    info = self.skill_boost_to_str(text)
+    if info:
+      embed.add_field(name = "**Skill Bonuses:**", value = info, inline = False)
+      info = ""
+
+    # getting description
+    info = self.format_desc(text, "description")
+    if info:
+      embed.add_field(name = "**Description**", value = info, inline = False)
+      info = ""
+
+    # getting flavor text
+    info = self.format_desc(text, "flavor text")
+    if info:
+      embed.add_field(name = "**Flavor Text**", value = "_{}_".format(info), inline = False)
+
+    embed.add_field(name = "\u3164", value = "Information provided by the DFO World Wiki\nRequested by {}".format(ctx.author.mention))
+
+    embed.set_footer(text = "Page ID: {}".format(item["pageid"]))
+    await ctx.send(embed = embed)
+
+  # grabs the first image found on the page -- this is usually the thumbnail
   def fetchThumbnail(self, url):
     r = requests.get(url)
     data = r.text
@@ -23,73 +123,65 @@ class Wiki(commands.Cog):
     imgSrc = link.get('src')
     links.append(imgSrc)
     thumb = links[0]
-    return thumb;
+    return "http://wiki.dfo.world" + thumb;
 
-  @commands.command(pass_context=True, description = "Pulls a page from the DFO World Wiki")
-  async def dfopedia(self, ctx, *, search_terms: str = None):
-    '''Searches the DFO Wiki.'''
+  # converts the wikitext string into a nicely formatted dict
+  def turn_into_dict(self, item):
+    text = item["wikitext"]["*"].split("\n| ")
+    attributes = {}
+    for item in text:
+      if "=" in item:
+        kv = item.split(" = ")
+        attributes[kv[0]] = kv[1].split("==")[0].replace("\n\n", "\n")
+    return attributes
 
-    if search_terms is None:
-      # catches null-answer
-      embed = discord.Embed(title ="", description = "Try asking a question with `!dfopedia <search_term>`, {}".format(ctx.author.mention))
-      await ctx.send(embed = embed)
+  # checks if a key in the provided item dict appears
+  #      return: if key exists, returns string data -- otherwise returns nothing
+  #        dict: provided dictionary
+  #         key: key to be checked
+  def exists_to_str(self, dict, key, *, plus: bool = False, percent: bool = False):
+    output = ""
+    if key in dict and dict[key]:
+      if key == "set":
+        output = "**Set:** [{}](http://wiki.dfo.world/view/{})".format(dict[key], dict[key].replace(" ", "_"))
+      else:
+        output = "**{}:** {}{}{}\n".format(self.replace[key] if key in self.replace else titlecase(key), "+" if plus else "", titlecase(dict[key]), "%" if percent else "")
+      emoji = discord.utils.get(self.bot.emojis, name = key.replace(" ", ""))
+      if emoji is None:
+        emoji = discord.utils.get(self.bot.emojis, name = "null")
+      output = "{}".format(emoji) + output
+      output = "\u3164" + output
+    return output
 
-    else:
-      # sanitizes the search term to make it URL friendly
+  # interprets skill boost information into a readable string
+  def skill_boost_to_str(self, dict):
+    output = ""
+    emoji = discord.utils.get(self.bot.emojis, name = "null")
+    if "all skill bonus" in dict:
+      info = dict["all skill bonus"]
+      info = info.split(",")
+      output += "\u3164{}All Level {} - {} Skills Lv. +{} (Special Skills excluded)".format(emoji, info[1], info[2], info[3])
+    if "single skill bonus" in dict:
+      info = dict["single skill bonus"]
+      info = info.replace("\n", "")
+      info = info.split(";")
+      for skill in info:
+        parsed_skill = skill.split(",")
+        output += "\u3164{}{} [{}](http://wiki.dfo.world/view/{}) Skill Lv. +{}\n".format(emoji, parsed_skill[0], parsed_skill[1][1:], parsed_skill[1][1:].replace(" ", "_"), parsed_skill[2][1:])
+    return output
 
-      search_terms = titlecase(search_terms)
-      term = search_terms.replace(" ", "_")
-      result = "http://wiki.dfo.world/view/index.php?search={}&title=Special%3ASearch&go=GO".format(term)
-
-      # grabs the first image on the page
-      thumb = self.fetchThumbnail(result)
-
-      # creates the message
-      embed = discord.Embed(title = search_terms, description = "Requested by **" + ctx.author.mention + "**\nPowered by the **[DFO World Wiki](http://wiki.dfo.world/view/Main_Page)**.", url = result, color = ctx.message.author.color)
-      embed.set_thumbnail(url = "http://wiki.dfo.world" + thumb)
-
-      try:
-        await ctx.send(embed = embed)
-
-      except:
-        # exception is raised if for some reason the page cannot be accessed
-        error = discord.Embed(title = "", description = "If Becky appears to the right, then the requested page does not exist. If she does not, then the [DFO World Wiki](http://wiki.dfo.world/view/Main_Page) may be offline. Either double-check your spelling, or try again later.")
-        error.set_author(name = "Whoops! I couldn't find {}!".format(search_terms))
-        error.set_thumbnail(url = "http://wiki.dfo.world/images/0/0a/Beckyportrait.png")
-        await ctx.send(embed = error)
-
-  @commands.command(pass_context=True, description = "Pulls a page from the Official Stardew Valley Wiki")
-  async def stardew(self, ctx, *, search_terms: str=None):
-    '''Searches the DFO Wiki.'''
-
-    if search_terms is None:
-      # catches null-answer
-      embed = discord.Embed(title ="", description = "Try asking a question with `!stardew <search_term>`, {}".format(ctx.author.mention))
-      await ctx.send(embed = embed)
-
-    else:
-      # sanitizes the search term to make it URL friendly
-
-      search_terms = titlecase(search_terms)
-      term = search_terms.replace(" ", "_")
-      result = "https://stardewvalleywiki.com/mediawiki/index.php?search={}&title=Special%3ASearch&go=GO".format(term)
-
-      # grabs the first image on the page
-      thumb = self.fetchThumbnail(result)
-
-      # creates the message
-      embed = discord.Embed(title = search_terms, description = "Requested by **" + ctx.author.mention + "**\nPowered by the **[Official Stardew Valley Wiki](https://stardewvalleywiki.com/Stardew_Valley_Wiki)**.", url = result, color = ctx.message.author.color)
-      embed.set_thumbnail(url = "https://stardewvalleywiki.com" + thumb)
-
-      try:
-        await ctx.send(embed = embed)
-
-      except:
-        # exception is raised if for some reason the page cannot be accessed
-        error = discord.Embed(title = "", description = "If Mayor Lewis appears to the right, then the requested page does not exist. If he does not, then the [Stardew Valley Wiki](https://stardewvalleywiki.com/Stardew_Valley_Wiki) may be offline. Either double-check your spelling, or try again later.")
-        error.set_author(name = "Whoops! I couldn't find {}!".format(search_terms))
-        error.set_thumbnail(url = "https://stardewvalleywiki.com/mediawiki/images/b/bb/Lewis_Happy.png")
-        await ctx.send(embed = error)
+  # interprets description and flavor text into a readable string
+  def format_desc(self, dict, param):
+    output = ""
+    if param == "description" and param in dict:
+      output = dict["description"].replace("\n\n", "\n").replace("<br>", "\n").replace("{{IconLink|", "").replace("}}", "").replace("[", "").replace("]", "")
+    if param == "flavor text" and param in dict:
+      output = dict["flavor text"].replace("\n\n", "\n").replace("<br>", "\n").replace("{{IconLink|", "").replace("}}", "").replace("[", "").replace("]", "")
+    while output.startswith("\n"):
+      output = output[1:]
+    while output.endswith("\n"):
+      output = output[:-2]
+    return output
 
 def setup(bot):
   bot.add_cog(Wiki(bot))
