822a88be3821fec6d177d9d4155dabf887e78aa5
snafuPop <krbingpop@gmail.com>
2019-09-09 18:52:37 +0000

    some small changes

--- modules/random.py
+++ modules/random.py
@@ -11,7 +11,7 @@ class Random(commands.Cog):
 
   # rolls a random number between 1 and a user defined max (defaults to 100)
   # also optionally takes in a string input
-  @commands.command(pass_context = True, description = "Generates a random number from 1 to a defined number (by default 100)")
+  @commands.command(description = "Generates a random number from 1 to a defined number (by default 100)")
   async def roll(self, ctx, number: int = 100, *, request: str = ""):
 
     if number <= 1:
@@ -20,11 +20,7 @@ class Random(commands.Cog):
     result = randint(1, number)
 
     # rolls a bonus modifier that determines the appearance of special dice or not
-    bonus = randint(1,1000000)
-    if bonus == 1:
-      dice = ":diamond_shape_with_a_dot_inside:"
-    else:
-      dice = ":game_die:"
+    dice = ":diamond_shape_with_a_dot_inside:" if 1 == randint(1,1000000) else ":game_die:"
 
     # parses the request
     if request is not "":
@@ -37,7 +33,7 @@ class Random(commands.Cog):
 
   # flips a coin
   # has a 100/101 chance of being heads or tails, and a 1/100 chance of landing on its side
-  @commands.command(pass_context = True, description = "Flips a coin")
+  @commands.command(description = "Flips a coin")
   async def flip(self, ctx):
 
     rand = randint(1, 101)
@@ -51,13 +47,9 @@ class Random(commands.Cog):
     await ctx.send(embed=embed)
 
   # randomly chooses from a list of user-defined options
-  @commands.command(pass_context = True, description = "Chooses from a list of options")
+  @commands.command(description = "Chooses from a list of options")
   async def choose(self, ctx, *choices):
-    if len(choices) < 2:
-      embed = discord.Embed(title = "", description = "I need more than one option. Try `{}choice <option1> <option2> ...`, {}".format(ctx.prefix, ctx.author.mention))
-    else:
-      embed = discord.Embed(title = "", description = "I choose **{}**, {}.".format(choice(choices), ctx.author.mention), color = ctx.author.color)
-    await ctx.send(embed = embed)
+    await ctx.send(embed = discord.Embed(title = "", description = "I need more than one option. Try `{}choice <option1> <option2> ...`, {}".format(ctx.prefix, ctx.author.mention)) if len(choices) < 2 else discord.Embed(title = "", description = "I choose **{}**, {}.".format(choice(choices), ctx.author.mention), color = ctx.author.color))
 
 def setup(bot):
   bot.add_cog(Random(bot))
