3164cc60d612d74e7f67ad589d6a3aab463fae67
snafuPop <krbingpop@gmail.com>
2021-09-14 21:03:02 -0400

    rewriting for slash lib

--- modules/random.py
+++ modules/random.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+from discord_slash import cog_ext, SlashContext, SlashCommand
+from discord_slash.utils import manage_components
+from discord_slash.model import ButtonStyle
+from discord_slash.utils.manage_commands import create_option, create_choice
 from builtins import bot
 
 from random import randint
@@ -11,45 +15,50 @@ class Random(commands.Cog):
 
   # rolls a random number between 1 and a user defined max (defaults to 100)
   # also optionally takes in a string input
-  @commands.command(description = "Generates a random number from 1 to a defined number (by default 100)")
-  async def roll(self, ctx, number: int = 100, *, request: str = ""):
-
+  @cog_ext.cog_slash(name = "roll", description = "Rolls a dice.", guild_ids = guild_ids, 
+    options = [create_option(
+      name = "number",
+      description = "The highest possible number you can roll. Leave blank for 100.",
+      option_type = 4,
+      required = False),
+    create_option(
+      name = "declaration",
+      description = "Attaches a declaration to your outcome.",
+      option_type = 3,
+      required = False)]
+    )
+  async def roll(self, ctx, number: int = 100, *, declaration: str = ""):
     if number <= 1:
-      await ctx.send(embed = discord.Embed(title = "", description = "Maybe roll a value greater than 1, {}? (｀Д´)".format(ctx.author.mention)))
+      await ctx.send(embed = discord.Embed(title = "", description = ":no_entry: Maybe roll a value greater than 1, {}? (｀Д´)".format(ctx.author.mention)))
       return
-    result = randint(1, number)
 
     # rolls a bonus modifier that determines the appearance of special dice or not
     dice = ":diamond_shape_with_a_dot_inside:" if 1 == randint(1,1000000) else ":game_die:"
 
-    # parses the request
-    if request is not "":
-      request = "\"" + request + "\""
-
     # creates the embed message
-    embed = discord.Embed(title = request, description = "{} {} rolled a **{:,}**! {}".format(dice, ctx.author.mention, result, dice), color = ctx.author.color)
-    embed.set_footer(text = "...out of {:,}.".format(number))
+    result = randint(1, number)
+    embed = discord.Embed(title = "", description = "{} {} rolled a **{:,}**! {}".format(dice, ctx.author.mention, result, dice), color = ctx.author.color)
+    embed.set_footer(text = "...out of {:,}".format(number))
+
+    # parses the declaration
+    if declaration != "":
+      declaration = "\"" + declaration + "\""
+      embed.set_author(name = declaration, icon_url = ctx.author.avatar_url)
+    
     await ctx.send(embed=embed)
 
-  # flips a coin
-  # has a 100/101 chance of being heads or tails, and a 1/100 chance of landing on its side
-  @commands.command(description = "Flips a coin")
-  async def flip(self, ctx):
 
+  @cog_ext.cog_slash(name = "coinflip", description = "Flips a coin.", guild_ids = guild_ids)
+  async def flip(self, ctx):
     rand = randint(1, 101)
     if rand == 1:
       result = "its side"
     elif rand >= 51:
       result = "tails"
     else:
-      result = "heads"
+      result = "heads"  
     embed = discord.Embed(description="{} flipped a coin and it landed on **{}**!".format(ctx.author.mention, result), color = ctx.author.color)
     await ctx.send(embed=embed)
-
-  # randomly chooses from a list of user-defined options
-  @commands.command(description = "Chooses from a list of options")
-  async def choose(self, ctx, *choices):
-    await ctx.send(embed = discord.Embed(title = "", description = "I need more than one option. Try `{}choice <option1> <option2> ...`, {}".format(ctx.prefix, ctx.author.mention)) if len(choices) < 2 else discord.Embed(title = "", description = "I choose **{}**, {}.".format(choice(choices), ctx.author.mention), color = ctx.author.color))
 
 def setup(bot):
   bot.add_cog(Random(bot))
