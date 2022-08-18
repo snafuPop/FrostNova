26babf7660a8e57c1833035ddab45b407e65659b
snafupop <snafupop@pm.me>
2022-08-18 02:02:24 +0000

    added global error handling

---
+++ cogs/error_handler.py
@@ -0,0 +1,26 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord.app_commands import AppCommandError
+
+import sys
+import traceback
+
+class CommandErrorHandler(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    bot.tree.on_error = self.on_app_command_error
+
+
+  async def on_app_command_error(self, interaction: discord.Interaction, error: AppCommandError):
+    if isinstance(error, app_commands.CheckFailure):
+      message = "You do not have the required permission(s) to run this command."
+      embed = self.bot.create_error_response(message = message)
+      await interaction.response.send_message(embed = embed, ephemeral = True)
+
+    else:
+      print('Ignoring exception in command {}:'.format(interaction))
+      traceback.print_exception(type(error), error, error.__traceback__)
+
+async def setup(bot: commands.Bot) -> None:
+  await bot.add_cog(CommandErrorHandler(bot))
