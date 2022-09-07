e2adf54fc9ac86428f983d9c2823b0ba52e0d649
snafupop <snafupop@pm.me>
2022-09-07 02:10:40 +0000

    removed old, commented out code

--- cogs/meme.py
+++ cogs/meme.py
@@ -65,48 +65,8 @@ class Meme(commands.Cog):
     await interaction.response.defer()
     message = textwrap.fill(message, width = 30)
     image_file = Image_Sans(message).make_image()
-    await interaction.followup.send(file = discord.File(fp = image_file, filename = "jesus.png"))
+    await interaction.followup.send(file = discord.File(fp = image_file, filename = "sans.png"))
 
-
-  # # controls making all of the images via PIL and running it thorugh BytesIO to make Discord happy
-  # def make_image(self, image_name, message, coords, color, font_name, font_size):
-  #   with Image.open(self.get_image_filepath() + image_name) as image:
-  #     draw = ImageDraw.Draw(image)
-  #     font = ImageFont.truetype(self.get_font_filepath() + font_name, font_size)
-  #     draw.text((coords[0], coords[1]), message, (color[0], color[1], color[2], color[3]), font = font)
-
-  #     output_buffer = BytesIO()
-  #     image.save(output_buffer, "png")
-  #     output_buffer.seek(0)
-  #   return output_buffer
-
-
-  # @cog_ext.cog_subcommand(base = "meme", name = "badcode", description = "Make fun of some code.", 
-  #   options = [create_option(
-  #     name = "code",
-  #     description = "The code to make fun of.",
-  #     option_type = 3,
-  #     required = True)])
-  # async def badcode(self, ctx, *, code:str = None):
-  #   await ctx.defer()
-  #   code = code.replace("`", "")
-  #   await ctx.send(file = discord.File(fp = self.make_image("comsci.png", code, [430, 25], [0, 0, 0, 255], "FantasqueSansMono-Regular.ttf", 20), filename = "badcode.png"))
-
-
-
-
-  # @cog_ext.cog_subcommand(base = "meme", name = "sans", description = "uhuhuhuhuhuhuhuh", 
-  #   options = [create_option(
-  #     name = "message",
-  #     description = "Any message.",
-  #     option_type = 3,
-  #     required = True)])
-  # @commands.command(description = "uhuhuhuhuhuhuhuh")
-  # async def sans(self, ctx, *, message: str = None):
-  #   await ctx.defer()
-  #   message = textwrap.fill(message, width = 30)
-  #   await ctx.send(file = discord.File(fp = self.make_image("sans.png", message, [117, 30], [255, 255, 255, 255], "UndertaleSans.ttf", 24), filename = "sans.png"))
-    
 
 async def setup(bot: commands.Bot) -> None:
   await bot.add_cog(Meme(bot))
